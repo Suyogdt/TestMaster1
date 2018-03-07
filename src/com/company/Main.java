3ed27174dad8fdb037f108f3d8a266ebd3b69470
Suyog <suyogdt@gmail.com>
2018-03-08 00:51:46 +0530

    Commit 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Dad");
-        System.out.println("Mom");
-        System.out.println("Son");
 
     }
 }
